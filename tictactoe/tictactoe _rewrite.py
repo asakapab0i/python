f127be6761e3c3bf27c05e89528c0c7139b9b61f
Bryan Bojorque <brbojorque@bitbucket.org>
2014-01-13 13:51:15 +1030

    rewrite using strict rules on oop

---
+++ tictactoe/tictactoe _rewrite.py
@@ -0,0 +1,11 @@
+#import random module
+
+class Tictactoe:
+    """A tictactoe game"""
+
+    #constructor
+    def __init__(self, player_instance, bot_instance):
+        self._player_instance = player_instance
+        self._bot_instanace = bot_instance
+
+        
