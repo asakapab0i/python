c4d178b3c30efa75c47969f01e4d4ac638d83f7f
Bryan Bojorque <brbojorque@bitbucket.org>
2014-01-13 14:03:02 +1030

    buggy game class

--- tictactoe/tictactoe _rewrite.py
+++ tictactoe/tictactoe _rewrite.py
@@ -7,5 +7,32 @@ class Tictactoe:
     def __init__(self, player_instance, bot_instance):
         self._player_instance = player_instance
         self._bot_instanace = bot_instance
+        self._board = range(0,10)
+        
+    def board(self):
+        print self._board[0], '|', self._board[2], '|', self._board[2]
+        print('----------')
+        print self._board[3], '|', self._board[4], '|', self._board[5]
+        print('----------')
+        print self._board[6], '|', self._board[7], '|', self._board[9]
 
+    def start(self):
+        self.board()
+
+        while not is_ended():
+            self.player_instance.on_turn()
+            self.bot_instance.on_onturn()
+
+    
+
+def main():
+    tictac = Tictactoe("Bryan","Bot")
+    tictac.start()
+
+if __name__ == "__main__":
+    main()
+        
+        
+        
+        
         
